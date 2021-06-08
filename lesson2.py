13cc14665d822040c0d0ec85a95fb36be5635466
evgenia2501 <83422740+evgenia2501@users.noreply.github.com>
2021-06-08 16:47:42 -0400

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,12 @@
+my_string = "Test Automation"
+word_test = ["Test"]
+print(word_test)
+
+my_string = "Test Automation"
+second_word = ['Automation']
+print(second_word)
+
+my_list = [10, 50, 20, 25, 30]
+elements = [10, 50, 20, 25,30]
+sliced_list = elements[1:-2]
+print(sliced_list)
